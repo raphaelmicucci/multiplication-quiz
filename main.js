27ad7de7dd0f6c9df68d53e50eb9ea9fa9b60b08
Raphael <raphaelmicucci@gmail.com>
2023-12-19 16:53:35 -0300

    round mechanism fixed

--- main.js
+++ main.js
@@ -2,38 +2,36 @@ let round = 1;
 let correct = 0;
 let incorrect = 0;
 
+let nums = [];
 let num1 = Math.ceil(Math.random()*10);
 let num2 = Math.ceil(Math.random()*10);
-
+nums.push([num1,num2]);
 
 window.onload = init;
-
 function init(){
-    document.getElementById("num1").innerText = num1;
-    document.getElementById("num2").innerText = num2;
+    document.getElementById("num1").innerText = nums[nums.length-1][0];
+    document.getElementById("num2").innerText = nums[nums.length-1][1];
 }
 
-
-const checkAnswer = (num1, num2) => {
-
+const checkAnswer = (nums) => {
     const answer = Number(document.getElementById("answer").value);
-
-    const correctAnswer = num1 * num2;
-
+    const correctAnswer = nums[round-1][0] * nums[round-1][1];
     answer == correctAnswer? correct++ : incorrect++;
 
-    console.log(`Round ${round}: ${num1} x ${num2} = ${correctAnswer}`);
-    
+    console.log(`Round ${round}: ${nums[round-1][0]} x ${nums[round-1][1]} = ${correctAnswer}`);
     
 
     if (round == 10) {
         document.querySelector('button').disabled = true;
+        document.querySelector('#answer').disabled = true;
     } else {
         round++;
+        num1 = Math.ceil(Math.random()*10);
+        num2 = Math.ceil(Math.random()*10);
+        nums.push([num1,num2]);
+
+        document.getElementById("num1").innerText = nums[nums.length-1][0];
+        document.getElementById("num2").innerText = nums[nums.length-1][1];
     }
 }
 
-
-
-// const num1 = document.getElementById("num1");
-// const num2 = document.getElementById("num2");
